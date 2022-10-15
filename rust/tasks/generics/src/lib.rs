aed43661b5e17f96e21724265e5f8b60bbafb907
Daria Iakimova <iakim.daria@gmail.com>
2022-10-15 22:46:45 +0900

    init generics

---
+++ rust/tasks/generics/src/lib.rs
@@ -0,0 +1,18 @@
+use std::fmt::format;
+
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, {} ({})", self.headline, self.author, self.location)
+    }
+}
