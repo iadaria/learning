51c4688d86b3969133ace5f71c5c9496b7e85215
iadaria <iakim.daria@gmail.com>
2022-10-29 17:54:24 +0900

    generic continue 2

--- rust/tasks/generics/src/lib.rs
+++ rust/tasks/generics/src/lib.rs
@@ -1,7 +1,9 @@
-use std::fmt::format;
-
 pub trait Summary {
     fn summarize(&self) -> String;
+    fn summarize_author(&self) -> String;
+    fn summarize_default(&self) -> String {
+        format!("(Reading further in {}...)", self.summarize_author())
+    }
 }
 
 pub struct NewsArticle {
@@ -14,5 +16,26 @@ pub struct NewsArticle {
 impl Summary for NewsArticle {
     fn summarize(&self) -> String {
         format!("{}, {} ({})", self.headline, self.author, self.location)
+    }
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
     }
 }
--- rust/tasks/generics/src/main.rs
+++ rust/tasks/generics/src/main.rs
@@ -1,3 +1,17 @@
+use generics::{Summary, Tweet};
+
+pub fn notify(item: impl Summary) {
+    println!("Express news! {}", item.summarize());
+}
+
 fn main() {
-    println!("Hello, world!");
+    let tweet = Tweet {
+        username: String::from("Dasha"),
+        content: String::from("of course, as you know, maybe, you know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+    println!("1 new tweet default: {}", tweet.summarize_default());
 }
