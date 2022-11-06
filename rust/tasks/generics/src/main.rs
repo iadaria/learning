a2fb6c8abc48407f46071f2324cff22880aa5f5d
Daria <iakim.daria@gmail.com>
2022-11-07 00:23:46 +0900

    _

--- rust/tasks/generics/src/main.rs
+++ rust/tasks/generics/src/main.rs
@@ -4,7 +4,50 @@ pub fn notify(item: impl Summary) {
     println!("Express news! {}", item.summarize());
 }
 
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, how are you"),
+        reply: false,
+        retweet: false,
+    }
+}
+
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_second<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
 fn main() {
+    let number_list = vec![34, 50, 25, 100, 54];
+
+    let result = largest(&number_list);
+    println!("\nThe biggest digit is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The lagest symbol is {}\n", result);
+
     let tweet = Tweet {
         username: String::from("Dasha"),
         content: String::from("of course, as you know, maybe, you know, people"),
@@ -12,6 +55,8 @@ fn main() {
         retweet: false,
     };
 
-    println!("1 new tweet: {}", tweet.summarize());
-    println!("1 new tweet default: {}", tweet.summarize_default());
+    println!("\n1 new tweet: {}", tweet.summarize());
+    println!("1 new tweet default: {}\n", tweet.summarize_default());
+
+    returns_summarizable();
 }
