9ea9053e53daa1ab6b2c4fcab2a8c1732c4dbdab
iadaria <jadarya@mail.ru>
2022-12-05 20:35:21 +0900

    callback 1/4

--- javascript/interview/book-kantor/async/callback.js
+++ javascript/interview/book-kantor/async/callback.js
@@ -1,7 +1,12 @@
-function loadScript(src) {
+// 1 part
+function loadScript(src, callback) {
   let script = document.createElement('script');
   script.src = src;
+  // 2 part
+  script.onload = () => callback(script);
   document.head.append(script);
 }
 
-loadScript('async/loadScript.js');
+loadScript('async/script.js', function () {
+  newFunction();
+});
---
+++ javascript/interview/book-kantor/async/script.js
@@ -0,0 +1,7 @@
+alert('The scrpit is executing');
+
+const newVariable = 'a new variable';
+
+function newFunction() {
+  console.log('New function is executing');
+}
