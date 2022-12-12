ee9d81048c404089a6457642e5642e5c84dc6e78
iadaria <jadarya@mail.ru>
2022-12-12 21:47:49 +0900

    init tetris

--- rust/tasks/guessing_game/src/main.rs
+++ rust/tasks/guessing_game/src/main.rs
@@ -1,67 +1,78 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 /* fn print_type_of<T>(_: &T) {
   print!("{}", std::any::type_name::<T>())
 } */
 
 pub struct Guess {
-  value: i32,
+    value: i32,
 }
 
 impl Guess {
-  pub fn new(value: i32) -> Guess {
-    if value < 1 || value > 100 {
-      panic!("The scrent number have to between 1 and 100, but got {}", value);
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!(
+                "The scrent number have to between 1 and 100, but got {}",
+                value
+            );
+        }
+
+        Guess { value }
     }
 
-    Guess { value }
-  }
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
 
-  pub fn value(&self) -> i32 {
-    self.value
-  }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
 }
 
 fn main() {
     println!("Guess the number!");
-    
+
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is {}", secret_number);
 
     loop {
+        println!("Please, enter your option!");
 
-      println!("Please, enter your option!");
-      
-      let mut value = String::new();
+        let mut value = String::new();
 
-      io::stdin().read_line(&mut value)
-        .expect("Can't read the row");
+        io::stdin()
+            .read_line(&mut value)
+            .expect("Can't read the row");
 
         let value: i32 = match value.trim().parse() {
-          Ok(num) => num,
-          Err(_) => continue,
-      };
-      //.expect("Please, enter a digit");
-      //print_type_of(&guess);
-      
-      /* if guess < 1 || guess > 100 {
-        println!("The secret number is between 1 and 100");
-        continue
-      }*/
-      println!("You make the number: {} ", value);
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        //.expect("Please, enter a digit");
+        //print_type_of(&guess);
 
-      let mut guess = Guess::new(value);
+        /* if guess < 1 || guess > 100 {
+          println!("The secret number is between 1 and 100");
+          continue
+        }*/
+        println!("You make the number: {} ", value);
 
+        let mut guess = Guess::new(value);
 
-      match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small number"),
-        Ordering::Greater => println!("Too large number"),
-        Ordering::Equal =>  {
-          println!("You win!");
-          break;
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small number"),
+            Ordering::Greater => println!("Too large number"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
-      }
-  }
+    }
 }
--- rust/tasks/tests/src/lib.rs
+++ rust/tasks/tests/src/lib.rs
@@ -1,5 +1,31 @@
 pub fn greeting(name: &str) -> String {
-    format!("Hello!")
+    format!("Hello! {}", name)
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!(
+                "The scrent number have to be more or equal 1, but got {}",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "The scrent number have to be less or equal 100, but got {}",
+                value
+            );
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
 }
 
 #[cfg(test)]
@@ -9,5 +35,19 @@ mod tests {
     fn greeting_contains_name() {
         let result = greeting("Dasha");
         assert!(result.contains("Dasha"), "expected `{}`", result);
+    }
+    #[test]
+    #[should_panic(expected = "The scrent number have to be less or equal 100, but got 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 3 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("2 + 3 != 4 "))
+        }
     }
 }
