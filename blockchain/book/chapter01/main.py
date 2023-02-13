98d972034b907c14fff7dad126d2b47ec0807515
iadaria <jadarya@mail.ru>
2023-02-14 06:37:12 +0900

    python

--- blockchain/book/chapter01/main.py
+++ blockchain/book/chapter01/main.py
@@ -1,19 +1,7 @@
-
 from ecc import FieldElement
 
-a = FieldElement(7, 13)
-b = FieldElement(6, 13)
 
-print('a == b', a == b)
-print('a == a', a == a)
+a = FieldElement(3, 13)
+b = FieldElement(1, 13)
 
-print('a != b', a != b)
-print('a != a', a != a)
-
-print('a + b', a + b)
-print('a - b', a - b)
-
-x = 12^7*77^49
-y = x / 97
-d = x - y * 91
-print('x={}, y={}, d={}', x, y, d)
+print(a**3 == b)
