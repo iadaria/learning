bdd8714435c5236381754b724944e7511f2f2a64
iadaria <jadarya@mail.ru>
2023-02-23 23:03:49 +0900

    add iter to minigrep

--- rust/tasks/minigrep/src/main.rs
+++ rust/tasks/minigrep/src/main.rs
@@ -3,12 +3,16 @@ use std::{env, process};
 use minigrep::{run, Config};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("There is a problem in the parsing the arguments: {}", err);
+        process::exit(1);
+    });
+    /* let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
         eprintln!("There is a problem in the parsing the arguments: {}", err);
         process::exit(1);
-    });
+    }); */
 
     println!("Searching '{}'", config.query);
     println!("In the file '{}'", config.filename);
