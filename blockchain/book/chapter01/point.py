f5749afb152a89f945d95c120acc304046d7eb14
iadaria <jadarya@mail.ru>
2023-03-09 19:39:08 +0900

    init inifinity

--- blockchain/book/chapter01/point.py
+++ blockchain/book/chapter01/point.py
@@ -4,6 +4,8 @@ class Point:
         self.y = y
         self.a = a
         self.b = b
+        if self.x is None and self.y is None:
+            return
         if y ** 2 != x ** 3 + a * x + b:
             error = '({}, {}) is not on the curve'.format(x, y)
             raise ValueError(error)
@@ -13,3 +15,14 @@ class Point:
     
     def __ne__(self, other):
         return not (self == other)
+    
+    def __add__(self, other):
+        if self.a != other.a or self.b != other.b:
+            error = 'Points {}, {} are not on the same curve'.format(self, other)
+            raise ValueError(error)
+        if self.x is None:
+            return other
+        if other.x is None:
+            return self
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None, self.a, self.b)
