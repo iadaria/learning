c2ebd9ac76e9334e0a20a3520996a02619cdda7c
iadaria <jadarya@mail.ru>
2024-09-16 20:25:41 +0400

    _

--- theory/js/test.js
+++ theory/js/test.js
@@ -1,1 +1,15 @@
-console.log(null === undefined)
+'use strict'
+let welcome;
+{
+  function welcome1() {
+    console.log("Привет!");
+  }
+}
+
+{
+  welcome = function () {
+    console.log("hi")
+  }
+}
+welcome();
+welcome1(); // Error: welcome is not defined
