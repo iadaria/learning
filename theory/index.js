9792d7164263134e352f83775fb4e5b9799376b2
iadaria <jadarya@mail.ru>
2024-09-24 00:46:48 +0400

    _

--- theory/index.js
+++ theory/index.js
@@ -1,9 +1,23 @@
-let map = [1 , 3, 4]
+function pow(x, n) {
+  let result = 1;
+  for(let i = 0; i < n; i++) {
+    result *= x;
+  }
+  return result;
+}
 
+function pow2(x, n) {
+  return (n === 1) ? x: x * pow2(x, n - 1)
+}
 
+function clien() {
+  let result = pow(2, 3);
 
-console.log(map.keys())
+  console.log(result)
 
-console.log(map.values())
+  let result2 = pow2(2, 3);
 
-console.log(map.entries())
+  console.log(result2)
+}
+
+clien()
